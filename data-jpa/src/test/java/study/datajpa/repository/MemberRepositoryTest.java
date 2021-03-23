6b97cca2523d5aaaa8763f11fbb4898181b523f1
psw <sangwookp9591@gmail.com>
2021-03-23 15:35:58 +0900

    SPRING DATA JPA DAY-2 파라미터 바인딩

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -137,6 +138,19 @@ class MemberRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for (MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNames(){
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("AAA",20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> usernameList = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+        for (Member member : usernameList) {
+            System.out.println("member = " + member);
         }
     }
 }
